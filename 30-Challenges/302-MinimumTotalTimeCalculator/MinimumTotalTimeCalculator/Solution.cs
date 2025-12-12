50ad01de92943642428c4a953d26665187073e7a
omazany <34654109+omazany@users.noreply.github.com>
2025-12-12 13:06:23 +0100

    Implementing the solution for the 302-MinimumTotalTimeCalculator

--- 30-Challenges/302-MinimumTotalTimeCalculator/MinimumTotalTimeCalculator/Solution.cs
+++ 30-Challenges/302-MinimumTotalTimeCalculator/MinimumTotalTimeCalculator/Solution.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace MinimumTotalTimeCalculator;
 
 /// <summary>
@@ -17,6 +19,42 @@ public class Solution : IMinimumTotalTimeCalculator
         // TODO: Implement the solution using a priority queue
         // Hint: Track when each cashier will be free
         // Always assign the next customer to the cashier that will be free earliest
-        throw new NotImplementedException();
+
+        PriorityQueue<int, int> cashierQueue = new();    
+
+        int customerIndex = 0; // currently processing customer index
+
+        // First distribute initial customers to available cashiers
+        for (int i = 0; i < cashiers && customerIndex < customers.Length; i++)
+        {
+            cashierQueue.Enqueue(customers[customerIndex], customers[customerIndex]);
+            customerIndex++;
+        }
+
+        long minimumTotalTime = 0;
+
+        // Process remaining customers, we will dequeue the cashier that will be free the earliest
+        // assign new customer to it and count the time needed to free that cashier
+        while (customerIndex < customers.Length)
+        {
+            // Get the cashier that will be free the earliest
+            cashierQueue.TryDequeue(out int currentTime, out _);
+
+            // update the time needed to free the cashier again after the processing the next customer
+            int nextFreeTime = currentTime + customers[customerIndex];
+
+            // Enqueue the updated time for this cashier
+            cashierQueue.Enqueue(nextFreeTime, nextFreeTime);
+
+            customerIndex++;
+        }
+
+        // Finally deque all cashiers to find the maximum time taken
+        while (cashierQueue.Count > 0)
+        {
+            minimumTotalTime =  cashierQueue.Dequeue();            
+        }
+        
+        return minimumTotalTime;
     }
 }
